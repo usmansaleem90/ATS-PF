3c0d6bd8780045eafd56659eb3223248be66a3fa
Your Name <you@example.com>
2023-10-08 23:41:14 -0700

    main

--- ATS/src/components/HelloWorld.vue
+++ ATS/src/components/HelloWorld.vue
@@ -5,7 +5,7 @@
 
       <div class="text-body-2 font-weight-light mb-n1">Welcome to</div>
 
-      <h1 class="text-h4 font-weight-bold">Programmer Force</h1>
+      <h1 class="text-h4 font-weight-bold">Programmer's Force</h1>
 
       <div class="text-body-1 font-weight-light mb-n1">Clone this repositry and make your own branch and push the code to the main origin after complete the task</div>
       <div class="text-body-1 font-weight-light mb-n1">Work on your own module</div>
